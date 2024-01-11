b672b3852d5ace5c74baac60635fce0bbfc0c572
LDCC_HWANG <wjdckd652@naver.com>
2024-01-11 18:17:35 +0900

    테이블로 변경

--- autoDocx/futureChemOrder.py
+++ autoDocx/futureChemOrder.py
@@ -9,12 +9,12 @@ class PurchaseRequestApp(QWidget):
         super().__init__()
         
         self.setWindowTitle("구매요구서 자동화")
-        self.setGeometry(100, 100, 600, 400)
+        self.setFixedSize(300, 930)
 
         self.tab_widget = QTabWidget(self)
         self.setup_tab1()
-        self.setup_tab2()
-        self.setup_tab3()
+        # self.setup_tab2()
+        # self.setup_tab3()
 
         layout = QVBoxLayout()
         layout.addWidget(self.tab_widget)
@@ -24,31 +24,32 @@ class PurchaseRequestApp(QWidget):
                
         tab1 = QWidget()
         self.tab_widget.addTab(tab1, "수동입력")
-
+        
         label = QLabel("구매요구서 양식:")
         self.file_path = QLineEdit(self)
         self.file_path.setReadOnly(True)
         button = QPushButton("Browse", self)
         button.clicked.connect(self.upload_form)
-
+    
+        
+        
+        labels = ["기관명", "기관코드", "핵종명[앞]", "핵종수량", "핵종명[뒤]", "모델명"]
+        manpower_labels = [f"투입인력 : {i:02d}:{j:02d}" for i in range(8, 18) for j in range(0, 60, 30) if not (i == 17 and j == 30)]        
+        # 투입인력 항목 추가
+        labels +=manpower_labels + ["구입 예정일"]
+        
         table = QTableWidget(self)
         table.setColumnCount(2)  # 2개의 열을 갖는 테이블
-        table.setHorizontalHeaderLabels(["Label", "Value"])
-
-        labels = ["기관명", "기관코드", "핵종명[앞]", "핵종수량", "핵종명[뒤]", "모델명"]
-        for i, label in enumerate(labels):
-            table.setItem(i, 0, QTableWidgetItem(label))
+        table.setRowCount(len(labels))  # 레이블의 수에 따라 테이블 행 수 설정
+        table.setHorizontalHeaderLabels(["명칭", "입력값"])
+        table.setColumnWidth(1, 150) 
+        # 테이블에 레이블과 편집 가능한 QLineEdit 위젯 추가
+        for i, labelT in enumerate(labels):
+            table.setItem(i, 0, QTableWidgetItem(labelT))
             table.setCellWidget(i, 1, QLineEdit(self))
 
-        # 투입인력 항목 추가
-        manpower_labels = [f"투입인력 : {i:02d}:{j:02d}" for i in range(8, 19) for j in range(0, 60, 30) if not (i == 17 and j == 30)]
-        for i, manpower_label in enumerate(manpower_labels):
-            table.setItem(len(labels) + i, 0, QTableWidgetItem(manpower_label))
-            table.setCellWidget(len(labels) + i, 1, QLineEdit(self))
-
-        # 구입 예정일 항목 추가
-        table.setItem(len(labels) + len(manpower_labels), 0, QTableWidgetItem("구입 예정일"))
-        table.setCellWidget(len(labels) + len(manpower_labels), 1, QLineEdit(self))
+        # Row 번호 숨기기
+        table.verticalHeader().setVisible(False)        
 
         generate_button = QPushButton("파일생성하기", self)
         generate_button.clicked.connect(self.generate_word_file)
@@ -59,19 +60,34 @@ class PurchaseRequestApp(QWidget):
         hbox_file_path.addWidget(self.file_path)
         hbox_file_path.addWidget(button)
         tab1_layout.addLayout(hbox_file_path)
-
+        
+        # 테이블 LayOut 등록 
         tab1_layout.addWidget(table)
-
+        
+        # 파일생성 버튼 등록 
         tab1_layout.addWidget(generate_button)
+   
 
-        tab1.setLayout(tab1_layout)
-
+        tab1.setLayout(tab1_layout) 
+    
+    # # 텍스트 변경 
+    def replace_text_in_cell(self, cell, key, value):
+        cell.text = cell.text.replace(f"{key}", value)
+    
+    # # 값받아오기 변경 
+    def get_value_from_table(self, row):
+        item = self.tab_widget.widget(0).layout().itemAt(row, 1)
+        if item and isinstance(item.widget(), QLineEdit):
+            return item.widget().text()
+        return ""
+    
+    # # 파일올리기 
     def upload_form(self):
         file_dialog = QFileDialog()
         file_path = file_dialog.getOpenFileName(self, "Select Word file", "", "Word files (*.docx)")[0]
         if file_path:
             self.file_path.setText(file_path)
-
+            
     def generate_word_file(self):
         template_path = self.file_path.text()
         if template_path:
@@ -126,14 +142,6 @@ class PurchaseRequestApp(QWidget):
         self.tab_widget.addTab(tab3, "검색 입력")
         # Implement Tab 3 UI and functionality here        
 
-    def replace_text_in_cell(self, cell, key, value):
-        cell.text = cell.text.replace(f"{key}", value)
-
-    def get_value_from_table(self, row):
-        item = self.tab_widget.widget(0).layout().itemAt(row, 1)
-        if item and isinstance(item.widget(), QLineEdit):
-            return item.widget().text()
-        return ""
 def main():
     app = QApplication(sys.argv)
     window = PurchaseRequestApp()
